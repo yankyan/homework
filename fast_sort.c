b5d1b5acca6e4a237c59dd146bfe2ec0b6f0c6b9
Sergey_Shestov <sergey-shestov@yandex.ru>
2024-12-11 18:15:59 +0300

    fastsort

--- fast_sort.c
+++ fast_sort.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #define MASS_LEN 10
-int input_array(int *n, int *array);
-void output_array(int n, int *array);
-void quicksort_array(int n, int *array);
-void heapsort_array(int n, int *array);
+int input_array(int *array);
+void output_array(int *array);
+void quicksort_array(int *array, int left, int right);
+void heapsort_array(int *array);
+void heapify(int *array, int n, int i);
 
 int main() {
-    int n, array[MASS_LEN];
-    if (input_array(&n, array) == 0) {
-        quicksort_array(n, array);
-        output_array(n, array);
-        heapsort_array(n, array);
-        output_array(n, array);
+    int array[MASS_LEN];
+    if (input_array(array) == 0) {
+        quicksort_array(array, 0, MASS_LEN - 1);
+        output_array(array);
+        heapsort_array(array);
+        output_array(array);
         return 0;
     }
 }
 
-int input_array(int *n, int *array) {
-    if (scanf("%d", n) != 1) {
-        printf("n/a");
-        return 1;
-    }
-    for (int i = 0; i < *n; i++) {
+int input_array(int *array) {
+    for (int i = 0; i < MASS_LEN; i++) {
         if (scanf("%d", &array[i]) != 1) {
             printf("n/a");
             return 1;
@@ -30,68 +27,83 @@ int input_array(int *n, int *array) {
     return 0;
 }
 
-void output_array(int n, int *array) {
-    for (int i = 0; i < n; i++) {
+void output_array(int *array) {
+    for (int i = 0; i < MASS_LEN; i++) {
         printf("%d ", array[i]);
     }
+    printf("\n");
 }
-void quicksort_array(int n, int *array) {
-    int i = 0, j = n - 1, pivot = array[n / 2];
-    do {
-        while (array[i] < pivot) {
-            i++;
+void quicksort_array(int *array, int left, int right) {
+    if (left < right) {
+        int i = left, j = right;
+        int pivot = array[(left + right) / 2];
+        do {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        // Рекурсивно сортируем левую и правую части
+        if (left < j) {
+            quicksort_array(array, left, j);
         }
-        while (array[j] > pivot) {
-            j--;
+        if (i < right) {
+            quicksort_array(array, i, right);
         }
-        if (i <= j) {
-            int tmp = array[i];
-            array[i] = array[j];
-            array[j] = tmp;
-            i++;
-            j--;
-        }
-    } while (i <= j);
-    if (j > 0) {
-        quicksort_array(j + 1, array);
-    }
-    if (i < n) {
-        quicksort_array(n - i, &array[i]);
     }
 }
-void heapsort_array(int n, int *array) {
-    int i, j, k, tmp;
-    for (i = n / 2 - 1; i >= 0; i--) {
-        k = i;
-        tmp = array[i];
-        for (; k * 2 + 1 < n; k = j) {
-            j = k * 2 + 1;
-            if (j + 1 < n && array[j] < array[j + 1]) {
-                j++;
-            }
-            if (tmp < array[j]) {
-                array[k] = array[j];
-            } else {
-                break;
-            }
-        }
-        array[k] = tmp;
+void heapsort_array(int *array) {
+    int n = MASS_LEN;
+    
+    // Построение кучи
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(array, n, i);
     }
-    for (i = n - 1; i >= 0; i--) {
-        tmp = array[0];
+
+    // Извлечение элементов из кучи
+    for (int i = n - 1; i > 0; i--) {
+        // Меняем корень (максимальный элемент) с последним элементом
+        int temp = array[0];
         array[0] = array[i];
-        for (k = 0, j = 1; j < i; k = j, j = 2 * k + 1) {
-            if (j + 1 < i && array[j] < array[j + 1]) {
-                j++;
-            }
-            if (array[k] < array[j]) {
-                tmp = array[k];
-                array[k] = array[j];
-                array[j] = tmp;
-            } else {
-                break;
-            }
-        }
-        array[i] = tmp;
+        array[i] = temp;
+
+        // Восстанавливаем кучу для оставшихся элементов
+        heapify(array, i, 0);
+    }
+}
+
+void heapify(int *array, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    // Если левый дочерний элемент больше корня
+    if (left < n && array[left] > array[largest]) {
+        largest = left;
+    }
+
+    // Если правый дочерний элемент больше самого большого элемента
+    if (right < n && array[right] > array[largest]) {
+        largest = right;
+    }
+
+    // Если самый большой элемент не корень
+    if (largest != i) {
+        int temp = array[i];
+        array[i] = array[largest];
+        array[largest] = temp;
+
+        // Рекурсивно восстанавливаем кучу для затронутого поддерева
+        heapify(array, n, largest);
     }
 }
